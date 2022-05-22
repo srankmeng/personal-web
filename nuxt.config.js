939944ed6ce9abd13e114a6454b6ffc6817ed8b9
srankmeng <Jtfwu28AN>
2022-05-23 00:10:04 +0700

    add github action & favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,6 +9,10 @@ export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
 
+  router: {
+    base: '/personal-deployment/'
+  },
+
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'Jaturong Duangchaiyusuk',
