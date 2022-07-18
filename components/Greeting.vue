9aaca2d648903c5545643d65d12079f5363d72e0
srankmeng <Jtfwu28AN>
2022-07-18 20:48:25 +1000

    adjust greeting

--- components/Greeting.vue
+++ components/Greeting.vue
@@ -4,22 +4,22 @@
       Hello & Welcome to my website
     </p>
     <p class="text-sm sm:text-xl my-4" style="text-indent: 32px">
-      I am a team lead & web developer who wants to improve my skill always. I
-      have several experiences with software development. For technical skills
-      such as frontend, backend, automated testing, and AWS services (EC2, S3,
-      Lambda, Cloudfront, etc.). I'm good on the frontend more than the backend
-      but I can do it as well. For other skills, I used to use the Agile process
-      in my projects which have planning, backlogs refinement, daily stand-up,
-      and retrospective. And I can use tools that are related to my tasks. In
+      I am a web developer who wants to improve my skill always. I have several
+      experiences with software development. For technical skills such as
+      frontend, backend, automated testing, and AWS services (EC2, S3, Lambda,
+      Cloudfront, etc.). I'm good on the frontend more than the backend but I
+      can do it as well. For other skills, I used to use the Agile process in my
+      projects which have planning, backlogs refinement, daily stand-up, and
+      retrospective. And I can use tools that are related to my tasks. In
       addition, I supported the team like a review code, coaching team members,
       and finding solutions when having some problems with delivering tasks on
       time for team success.
     </p>
     <p class="text-sm sm:text-xl my-4" style="text-indent: 32px">
       I am overseas to study English in Brisbane, Australia for 6 months (August
-      to January from Monday to Friday 8.00 AM - 14.00 PM.) So I am looking for
-      a part-time job after the classes. Please feel free to contact me, thank
-      you all.
+      to January from Monday to Friday 8.00 AM - 2.00 PM.) So I am looking for a
+      part-time job after the classes. Please feel free to contact me, thank you
+      all.
     </p>
   </div>
 </template>
