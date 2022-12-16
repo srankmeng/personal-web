5e05b983392204c13002ec7f67043b46f8a9efd2
srankmeng <Jtfwu28AN>
2022-12-17 01:16:16 +1000

    Change greeting and pdf

--- components/Greeting.vue
+++ components/Greeting.vue
@@ -2,10 +2,15 @@
   <div class="greeting container mx-auto my-8 px-4 z-10">
     <p class="font-bold text-xl sm:text-3xl text-gray-900">Overview</p>
     <p class="text-sm sm:text-xl my-4" style="text-indent: 32px">
-      I am a web developer in Thailand. For technical skills such as frontend,
-      backend, automated testing, and AWS services (EC2, S3, Lambda, Cloudfront,
-      etc.). I'm good on the frontend more than the backend but I can do it as
-      well.
+      I have experience with 10+ years of Web development. For technical skills
+      such as frontend, backend, automated testing, and AWS services (EC2, S3,
+      Lambda, Cloudfront, etc.). I'm good on the frontend more than the backend
+      but I can do it as well. For other skills, I used to use the Agile process
+      in my projects which have planning, backlog refinement, daily stand up,
+      and retrospective. And I can use tools that are related to my tasks. In
+      addition, I supported the team like a review code, coaching team members,
+      and finding solutions when having problems delivering tasks on time for
+      team success.
     </p>
     <p class="text-sm sm:text-xl my-4" style="text-indent: 32px">
       Please feel free to contact me, thank you all.
