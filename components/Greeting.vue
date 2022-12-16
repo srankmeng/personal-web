b690b0f4f2673f593fc76092ad76d7872516e066
srankmeng <Jtfwu28AN>
2022-12-17 01:10:54 +1000

    Change greeting

--- components/Greeting.vue
+++ components/Greeting.vue
@@ -2,22 +2,13 @@
   <div class="greeting container mx-auto my-8 px-4 z-10">
     <p class="font-bold text-xl sm:text-3xl text-gray-900">Overview</p>
     <p class="text-sm sm:text-xl my-4" style="text-indent: 32px">
-      I am a web developer who wants to improve my skill always. I have several
-      experiences with software development. For technical skills such as
-      frontend, backend, automated testing, and AWS services (EC2, S3, Lambda,
-      Cloudfront, etc.). I'm good at the frontend more than the backend but I
-      can do it as well. For other skills, I used to use the Agile process in my
-      projects which have planning, backlogs refinement, daily stand-up, and
-      retrospective. And I can use tools that are related to my tasks. In
-      addition, I supported the team like a review code, coaching team members,
-      and finding solutions when having some problems with delivering tasks on
-      time for team success.
+      I am a web developer in Thailand. For technical skills such as frontend,
+      backend, automated testing, and AWS services (EC2, S3, Lambda, Cloudfront,
+      etc.). I'm good on the frontend more than the backend but I can do it as
+      well.
     </p>
     <p class="text-sm sm:text-xl my-4" style="text-indent: 32px">
-      I am overseas to study English in Brisbane, Australia for 6 months (August
-      to January from Monday to Friday 8.00 AM - 2.00 PM.) So I am looking for a
-      part-time job after the classes. Please feel free to contact me, thank you
-      all.
+      Please feel free to contact me, thank you all.
     </p>
   </div>
 </template>
